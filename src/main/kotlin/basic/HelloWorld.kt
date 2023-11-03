9dce20d00bd52ae155e0caaa5a5033a28f1f172f
da9dac <da9dac@gmail.com>
2023-11-03 16:39:13 +0900

    HelloWorld

--- src/main/kotlin/basic/HelloWorld.kt
+++ src/main/kotlin/basic/HelloWorld.kt
@@ -1,5 +1,25 @@
 package basic
 
+val popcorn = 5
+var hotdog = 7
 fun main() {
     println("Hello, world!")
+
+//    popcorn = 50  // val 변수는 쓰기가 불가능
+    hotdog = 70
+    println(hotdog)
+
+    printNumber()
+    printStringTemplates()
+}
+
+fun printNumber() {
+    println(popcorn)
+    println(hotdog)
+}
+
+fun printStringTemplates() {
+    println("핫도그를 $hotdog 개 시켰다.")
+    println("핫도그를 ${hotdog}개 시켰다.")
+    println("핫도그를 ${hotdog + 10}개 시켰다.")
 }
