dc4f0bedae993d2163effddd573a52f53e9b8578
da9dac <da9dac@gmail.com>
2023-11-03 17:28:28 +0900

    BasicTypes

--- src/main/kotlin/basic/BasicTypes.kt
+++ src/main/kotlin/basic/BasicTypes.kt
@@ -1,6 +1,48 @@
 package basic
 
-
 fun main() {
-    println("Test")
+    var customers = 10
+    println(customers)
+
+    customers = 8
+    println(customers)
+
+    customers = customers + 3
+    println(customers)
+
+    customers += 7
+    println(customers)
+
+    customers -= 3
+    println(customers)
+
+    customers *= 2
+    println(customers)
+
+    customers /= 3
+    println(customers)
+
+    val d: Int
+    d = 3
+
+    val e: String = "hello"
+
+    println(d)
+    println(e)
+
+    val aa = 1000
+    val bb = "log message"
+    val cc = 3.14
+    val dd = 100_000_000_000_000
+    val ee = false
+    val ff = '\n'
+
+    /*
+        val aa: Int = 1000
+        val bb: String = "log message"
+        val cc: Double = 3.14
+        val dd: Long = 100_000_000_000
+        val ee: Boolean = false
+        val ff: Char = '\n'
+     */
 }
